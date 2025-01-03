e86467a98de8f59b05326aa1a851a16416f4d8c8
Bakyt Niiazaliev <bakyt.worker@gmail.com>
2025-01-03 17:56:26 +0700

    feat(archive): complete task to try zip single file

--- archive/docs/tasks.md
+++ archive/docs/tasks.md
@@ -1,4 +1,4 @@
 # Tasks
 ## 03.12.2024
-- [ ] Try to archive a single file with Zip.
+- [X] Try to archive a single file with Zip.
 - [ ] Try to archive multiple files with Zip
--- archive/src/main.rs
+++ archive/src/main.rs
@@ -1,19 +1,33 @@
+use std::fs::File;
+
+use zip::write::SimpleFileOptions;
+
 extern crate clap;
 extern crate zip;
 
-use clap::Parser;
+// use clap::Parser;
 
-#[derive(Parser)]
-struct Args {
-    paths: Vec<String>,
-    // #[arg(short, long)]
-    // output: Option<String>,
+// #[derive(Parser)]
+// struct Args {
+//     paths: Vec<String>,
+// }
+
+fn archive_cargo_toml() {
+    let out = File::create("archive.zip").unwrap();
+    let mut zip = zip::ZipWriter::new(out);
+    let options = SimpleFileOptions::default().compression_method(zip::CompressionMethod::Stored);
+    zip.start_file("Cargo.toml", options).unwrap();
+
+    let mut file = File::open("Cargo.toml").unwrap();
+    std::io::copy(&mut file, &mut zip).unwrap();
+    zip.finish().unwrap();
 }
 
 fn main() {
-    let args = Args::parse();
-    if args.paths.is_empty() {
-        eprintln!("No paths provided");
-        return;
-    }
+    // let args = Args::parse();
+    // if args.paths.is_empty() {
+    //     eprintln!("No paths provided");
+    //     return;
+    // }
+    archive_cargo_toml();
 }
