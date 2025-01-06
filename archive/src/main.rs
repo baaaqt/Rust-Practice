803ee9dcc893b17b5c69618fdebdfe36f912e3d9
Bakyt Niiazaliev <bakyt.worker@gmail.com>
2025-01-07 03:45:04 +0700

    feat(archive): enhance CLI for archiving with output path and input validation

--- archive/src/main.rs
+++ archive/src/main.rs
@@ -1,33 +1,75 @@
-use std::fs::File;
+use std::{
+    fs::File,
+    io::{stdout, Write},
+    path::{Path, PathBuf},
+};
 
 use zip::write::SimpleFileOptions;
 
 extern crate clap;
 extern crate zip;
 
-// use clap::Parser;
+use clap::Parser;
 
-// #[derive(Parser)]
-// struct Args {
-//     paths: Vec<String>,
-// }
+#[derive(Parser)]
+struct Args {
+    /// FIles or directories to archive
+    paths: Vec<PathBuf>,
 
-fn archive_cargo_toml() {
-    let out = File::create("archive.zip").unwrap();
+    /// Output file path
+    #[arg(short, long, default_value = "Archive.zip")]
+    output_path: PathBuf,
+}
+
+fn check_if_paths_is_not_empty(paths: &[PathBuf]) -> Result<(), &'static str> {
+    if paths.is_empty() {
+        return Err("No paths provided");
+    }
+    Ok(())
+}
+
+fn archive(output: &Path, fp: &Path) {
+    let out = File::create(output).unwrap();
     let mut zip = zip::ZipWriter::new(out);
     let options = SimpleFileOptions::default().compression_method(zip::CompressionMethod::Stored);
-    zip.start_file("Cargo.toml", options).unwrap();
+    zip.start_file(fp.file_name().unwrap().to_str().unwrap(), options)
+        .unwrap();
 
-    let mut file = File::open("Cargo.toml").unwrap();
+    let mut file = File::open(fp).unwrap();
     std::io::copy(&mut file, &mut zip).unwrap();
     zip.finish().unwrap();
 }
 
 fn main() {
-    // let args = Args::parse();
-    // if args.paths.is_empty() {
-    //     eprintln!("No paths provided");
-    //     return;
-    // }
-    archive_cargo_toml();
+    let mut args = Args::parse();
+    if let Err(e) = check_if_paths_is_not_empty(&args.paths) {
+        eprintln!("{}", e);
+        return;
+    }
+
+    if args.output_path.exists() && args.output_path.is_file() {
+        print!(
+            "Output file \"{}\" already exists, overwrite? [y]es, [n]o, [N]ew output file: ",
+            args.output_path.display()
+        );
+        stdout().flush().unwrap();
+        let mut buf = String::new();
+        std::io::stdin().read_line(&mut buf).unwrap();
+        match buf.trim() {
+            "y" => {}
+            "n" => return,
+            "N" => {
+                print!("Enter new output file path: ");
+                stdout().flush().unwrap();
+                buf.clear();
+                std::io::stdin().read_line(&mut buf).unwrap();
+                args.output_path = PathBuf::from(buf.trim());
+            }
+            _ => {
+                eprintln!("Invalid input");
+                return;
+            }
+        }
+    }
+    archive(&args.output_path, &args.paths[0]);
 }
